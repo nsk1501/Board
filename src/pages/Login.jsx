d23e26a8e029c9a8b581748d51ce900950379e51
nsk1501 <nsk1501@naver.com>
2024-03-08 21:52:22 +0900

    Design login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,19 +17,26 @@ const Login =() => {
     
 
     return(
-        <div>
-            <h2>login</h2>
-        <div>
-            <label htmlFor='input_id'>ID: </label>
-            <input type='text' name='input_id' value={inputId} onChange={handleInputId} />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{position:'absolute' }}>
+            <img src='/images/white-board.svg' style={{ width: '500px', height: '500px',marginTop:'700px' }} />
         </div>
-        <div>
-            <label htmlFor='input_pw'>PW: </label>
-            <input type='password' name='input_pw' value={inputPw} onChange={handleInputPw} />
-        </div>
-        <div>
-            <button type='button' onClick={onClickLogin}>Login</button>
-        </div>
+        <div style={{position:'absolute', marginTop:'500px'}}>
+            <h2 style = {{marginLeft : '65px'}}>login</h2>
+            <div style={{display:'flex'}} >
+                <div style={{display:'flex', flexDirection:'column'}}>
+                    <label htmlFor='input_id'>ID: </label>
+                    <label htmlFor='input_pw'>PW: </label>
+                </div>
+                <div style={{display:'flex', flexDirection:'column'}}>
+                    <input type='text' name='input_id' value={inputId} onChange={handleInputId} />
+                    <input type='password' name='input_pw' value={inputPw} onChange={handleInputPw} />
+                </div>
+            </div>
+            <div>
+                <button type='button' onClick={onClickLogin}>Login</button>
+            </div>
+        </div> 
     </div>
     )
 }
