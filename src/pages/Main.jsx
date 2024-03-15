30b61f39169b731993488091ef3e2d8ca42b5984
nsk1501 <nsk1501@naver.com>
2024-03-15 22:03:07 +0900

    Add main page contents

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,38 @@
-import React from "react";
 
 const Main = () => {
     return (
         <div>
-            야호라잉이잉이이이이이 첫게시판 기분좋아용
+            <h1>승권이가 추천하는 안양 맛집 정보</h1>
+            <h2>한식</h2>
+            <ul>
+                <li>신화 수산</li>
+                <li>큰 손</li>
+                <li>용기 곱창</li>
+            </ul>
+            <h2>양식</h2>
+            <ul>
+                <li>매드포갈릭</li>
+                <li>투파인드 피터</li>
+                <li>포크너</li>
+            </ul>
+            <h2>중식</h2>
+            <ul>
+                <li>중식당 125</li>
+                <li>호랑이 선생</li>
+                <li>화차이</li>
+            </ul>
+            <h2>일식</h2>
+            <ul>
+                <li>잔잔</li>
+                <li>오,늘의 초밥</li>
+                <li>파랑새야</li>
+            </ul>
+            <h2>승권이 최애 맛집 '👑Top 3👑'</h2>
+            <ul>
+                <li>👑Top 1👑 신화 수산</li>
+                <li>👑Top 2👑투파인드피터</li>
+                <li>👑Top 3👑 파랑새야</li>
+            </ul>
         </div>
     )
 }
