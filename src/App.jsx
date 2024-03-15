2d1521a1d51e74a9a07e6e5de63ac8cad2b41f30
nsk1501 <nsk1501@naver.com>
2024-03-15 20:44:29 +0900

    Add router, Make login function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Main from './pages/Main';
 
 
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='main' element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 const Login =() => {
     const [inputId, setInputId] = useState('')
-    const [inputPw, setInputPw] = useState('')
+    const [inputPw, setInputPw] = useState('')    
     
-    
+
     const handleInputId = (e) => {
         setInputId(e.target.value)
     }
@@ -13,13 +13,16 @@ const Login =() => {
     }
     const onClickLogin = () => {
         console.log('click login')
+        if (inputId==='testid' && inputPw==='testpw'){
+            window.location.href='/main'
+        } else {alert('❎일치하는 정보가 없습니다.❎') }
     }
     
 
     return(
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <div style={{position:'absolute' }}>
-            <img src='/images/white-board.svg' style={{ width: '500px', height: '500px',marginTop:'700px' }} />
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{ width:'100%',background:'black', position:'absolute'}}>
+            <img src='/images/white-board.svg' style={{ width: '500px', height: '500px', marginTop:'700px',marginLeft:'400px' }} />
         </div>
         <div style={{position:'absolute', marginTop:'500px'}}>
             <h2 style = {{marginLeft : '65px'}}>login</h2>
